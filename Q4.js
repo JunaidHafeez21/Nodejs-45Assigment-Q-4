ad6797a06dce909f381cc34add25c1d99355a057
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:22:11 +0500

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,5 @@
+"use strict";
+// Q4.Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+let name1 = ("Hazzrat Muhammad S.A.W said");
+console.log(`${name1}  ❝Namaz is the cooling of my eyes❞ `);
